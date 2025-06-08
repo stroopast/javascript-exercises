c3e4024b793d0c9ac0e3578edfd7b62cf0049945
Roberto <nanuroberto20@gmail.com>
2025-06-08 15:46:45 +0300

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = parseInt(n);
+
+    if(n < 0)
+    {
+        return "OOPS";
+    }
+
+    return clacFib(n);
 
 };
 
+function clacFib(n)
+{
+    if(n <= 1)
+    {
+        return n;
+    }
+
+    return clacFib(n - 1) + clacFib(n - 2);
+}
 // Do not edit below this line
 module.exports = fibonacci;
