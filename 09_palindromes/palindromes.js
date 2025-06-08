d84debcefa47da878667eb4bcda65ba03d6b8095
Roberto <nanuroberto20@gmail.com>
2025-06-08 15:34:44 +0300

    Solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    let newStr = ""
+    for(let i = 0; i < str.length; i++)
+    {
+        if(str.charCodeAt(i) >= 32 && str.charCodeAt(i) <= 47)
+        {
+            continue;
+        }
+        newStr += str[i];
+    }
 
+    let palindrome = "";
+    for(let i = newStr.length - 1; i >= 0; i--)
+    {
+        palindrome += newStr[i];
+    }
+    return palindrome === newStr;
 };
 
 // Do not edit below this line
